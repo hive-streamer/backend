32fe304297eed97a53fbfc5bc7c0fc1f2f9d2434
Michael Smith <selabie68@gmail.com>
2020-05-17 10:42:54 +1000

    Fixed restart shell script

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -1,19 +1,9 @@
 #!/usr/bin/env sh
-
-# Make sure Nginx configuration file is correct
-/usr/sbin/nginx -t -c /etc/nginx/nginx.conf
+nginx -t -c /etc/nginx/nginx.conf
 
 if [[ $? != 0 ]]; then 
     echo "Error: Nginx's configuration file contains some errors."
     exit 1; 
 fi
 
-/etc/init.d/nginx reload
-
-# Make sure Nginx is running or display the error message
-if [ -e /var/run/nginx.pid ]; then
-    echo "Nginx restarted successfully."
-else
-    echo "Nginx failed to restart."
-    exit 1;
-fi
+nginx -s reload
