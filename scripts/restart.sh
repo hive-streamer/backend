6980fb0c89e18d1faae5049ade7c49fd708e46f8
Michael Smith <selabie68@gmail.com>
2020-05-17 14:27:47 +1000

    PIpe to null to avoid permissions error

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
-nginx -t -c /etc/nginx/nginx.conf
+nginx -t -c /etc/nginx/nginx.conf > /dev/null 2>&1
 
-if [[ $? != 0 ]]; then 
+if [[ $? != 0 ]]; then
     echo "Error: Nginx's configuration file contains some errors."
-    exit 1; 
+    exit 1;
 fi
 
 nginx -s reload
